719041d5525efe2ee704c28acd8b50311da104fb
shrutiparsodkar02 <2023bit507@sggs.ac.in>
2024-01-18 00:28:49 +0530

    code for squareroot

---
+++ calci.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.Math;
+public class calci {
+
+    public static void main(String[] args) {
+        int n1=10;
+        int n2=5;
+        
+        //squareroot of numbers
+        System.out.println(Math.pow(n1, 2));
+
+    }
+}
