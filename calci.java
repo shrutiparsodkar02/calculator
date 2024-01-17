734d9cc4f6bab7883b4aa5d65b175251c7ec21f2
shrutiparsodkar02 <2023bit507@sggs.ac.in>
2024-01-17 23:38:45 +0530

    code for square added

---
+++ calci.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class calci {
+
+    public static void main(String[] args) {
+        int n1=10;
+        int n2=5;
+        //square of two numbers
+        System.out.println(n1*n1);
+
+    }
+}
