d7a63dffe36034ade65d858c437fc080622770a0
shrutiparsodkar02 <2023bit507@sggs.ac.in>
2024-01-17 23:18:09 +0530

    new code for multiplication

---
+++ calci.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class calci {
+
+    public static void main(String[] args) {
+        int n1=10;
+        int n2=5;
+        //multiplication of two numbers
+        System.out.println(n1*n2);
+
+    }
+}
