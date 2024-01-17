2dd8bc9b8144f480ad647205ac34154e25a92e4c
shrutiparsodkar02 <2023bit507@sggs.ac.in>
2024-01-17 23:07:00 +0530

    editied subtraction code

--- calci.java
+++ calci.java
@@ -1,1 +1,11 @@
- System.out.println(n1-n2);
+import java.io.*;
+public class calci {
+
+    public static void main(String[] args) {
+        int n1=10;
+        int n2=5;
+        //addition of two numbers
+        System.out.println(n1-n2);
+
+    }
+}
